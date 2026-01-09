cc95f23346b1a72ff0a0d6f8659051e8cd75fe01
hekciu <jakublasecki321@gmail.com>
2026-01-09 23:56:08 +0100

    slow fight with usb

--- c/usb.c
+++ c/usb.c
@@ -4,8 +4,11 @@
 
 void initialize_usb() {
     // activate register macrocell clock
+    SET_BIT(RCC->CFGR, BIT(22));
 
     // de-assert macrocell specific reset signal
+    SET_BIT(RCC->ABP1RSTR, !BIT(23));
+    SET_BIT(RCC->ABP1ENR, BIT(23));
 
     // switch on interval voltage for port transceiver
     SET_BIT(USB->CNTR, !BIT(1)); // !PDWN
@@ -26,4 +29,13 @@ void initialize_usb() {
 static void on_usb_reset() {
     // enable USB device
     SET_BIT(USB->DADDR, BIT(7)); // EF
+
+}
+
+
+static void configure_0_endpoint() {
+    // 0 endpoint must always have CONTROL type
+    USB->EP0R &= ~(3UL << 9);
+
+    // set USB KIND?
 }
